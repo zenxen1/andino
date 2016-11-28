a7b8027aea06f0aa5f2a3ba3a8ebf84d6ba2de54
sytnadosong <sytnado.song@gmail.com>
2016-11-28 14:43:47 +0900

    161128 14:43 디자인 영택 파트

---
+++ MyApplication/app/src/main/java/com/sds/study/andino/AddFriendItem.java
@@ -0,0 +1,24 @@
+package com.sds.study.andino;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+/**
+ * Created by song_kang on 2016-11-27.
+ */
+
+public class AddFriendItem extends LinearLayout {
+    Context context;
+    public AddFriendItem(Context context) {
+        super(context);
+        this.context=context;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.addfrienditem,this);
+    }
+
+    public void addFriend(){
+
+    }
+}
