2e28c2972a77ba6fcc5819b8a97389a561ba3028
M120216\efro2 <jsuk333>
2016-11-28 17:41:25 +0900

    ...

--- MyApplication/app/src/main/java/com/sds/study/andino/ChatActivity.java
+++ MyApplication/app/src/main/java/com/sds/study/andino/ChatActivity.java
@@ -9,15 +9,26 @@ import android.widget.ListView;
  * Created by efro2 on 2016-11-25.
  */
 
-public class JsActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
     ListView listView;
     BaloonAdapter baloonAdapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //-----------------------------------roomchat에서 인텐트 받아오기
+        //room_id,자신의 member_id,같이 채팅하는 사람들 id를 받아와서 db에서 이미지 정보등을 받아온다.
+
+        //-------------------------------------------------------
         setContentView(R.layout.jsactivity_layout);
         listView=(ListView)findViewById(R.id.listView);
         BaloonAdapter baloonAdapter=new BaloonAdapter(this);
         listView.setAdapter(baloonAdapter);
     }
+    //---------------서버 접속
+
+    //-----------------------------스트림 획득
+
+    //-----------------------------------채팅버튼 눌렀을 시 갱신
+
+    //------------------------------------
 }
