d0294b031974ead5e5e90b608dede5a74a069402
SeungHyeon\김승현 <ksh88724@naver.com>
2016-11-30 17:23:29 +0900

    ㄱㄱ

--- MyApplication/app/src/main/java/com/sds/study/andino/activity/LoginActivity.java
+++ MyApplication/app/src/main/java/com/sds/study/andino/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.sds.study.andino.R;
 
@@ -14,21 +15,38 @@ import com.sds.study.andino.R;
  */
 
 public class LoginActivity extends AppCompatActivity {
-
+    String TAG;
     Button bt,login_to_regist;
+    EditText idInput, passInput;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        TAG=getClass().getName();
         setContentView(R.layout.login_layout);
+
+
 
         bt=(Button) findViewById(R.id.login_to_main);
         login_to_regist=(Button)findViewById(R.id.login_to_regist);
+        idInput=(EditText)findViewById(R.id.idInput);
+        passInput=(EditText)findViewById(R.id.passwordInput);
+
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //로그인 누르면 서버를 통해 DB의 회원가입 상황과 비교
+
+               /* StringBuffer sb = new StringBuffer();
+                sb.append("{");
+                sb.append("\"title\":\"login\",");
+                sb.append("\"id\":\""+idInput.getText().toString()+"\",");
+                sb.append("\"password\":\""+passInput.getText().toString()+"\"");
+                sb.append("}");
+                //RoomActivity.clientThread.sendMsg(sb.toString());*/
+
                 Intent intent = new Intent(getApplicationContext(), RoomActivity.class);
                 startActivity(intent);
             }
